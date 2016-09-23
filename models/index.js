98733335c5c65bc28fd270fe20973269441fd867
rhamill1 <rhamill11@gmail.com>
2016-09-23 16:41:24 -0700

    adds heroku mongo back

--- models/index.js
+++ models/index.js
@@ -4,4 +4,5 @@ var mongoose = require("mongoose");
 module.exports.Book = require("./book.js");
 module.exports.Profile = require("./profile.js");
 
-mongoose.connect( "mongodb://localhost/personal-api");
+// mongoose.connect( "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
