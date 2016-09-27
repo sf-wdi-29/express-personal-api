5ae433735634c8f5b920893ee0d7931068cfc2f0
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-09-27 00:14:06 -0700

    adds ajax method to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,20 @@
 console.log("Sanity Check: JS is working!");
+var template;
+var $vinylsList;
+var allVinyls = [];
 
 $(document).ready(function(){
 
-// your code
+  $vinylsList = $('#vinylTarget');
+
+  var source = $('#vinyl-template').html();
+  template = handlebars.compile(source);
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/vinyls',
+    success: handleSuccess,
+    error: handleError
+  });
 
 });
